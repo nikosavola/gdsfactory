965e12c54c10c6ebc5025910854e68306b4fa8cb
Niko Savola <nikosavola@google.com>
2023-07-19 17:14:01 -0700

    Infer bodies and signals to use in electrostatic simulations
    * Improve Elmer sif-output
    * Define CDict for stroing results

--- gdsfactory/simulation/elmer/get_capacitance.py
+++ gdsfactory/simulation/elmer/get_capacitance.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 import shutil
 import subprocess
+import itertools
 from math import inf
 from pathlib import Path
-from typing import Dict, Any, List, Sequence, Optional
+from typing import Dict, Any, Tuple, Optional, Iterable
 
+from pandas import read_csv
 from numpy import isfinite
 from jinja2 import Environment, FileSystemLoader
 from pydantic import BaseModel
@@ -19,13 +21,35 @@ from gdsfactory.typings import MaterialSpec
 ELECTROSTATIC_SIF = "electrostatic.sif"
 ELECTROSTATIC_TEMPLATE = Path(__file__).parent / f"{ELECTROSTATIC_SIF}.j2"
 
+# TODO put to typings?
+CDict = Dict[Tuple[str, str], float]
+
 
 class ElectrostaticResults(BaseModel):
     """Results class for electrostatic simulations."""
 
-    capacitance_matrix: List[List[float]]
+    capacitance_matrix: CDict  # List[List[float]]  # TODO CDICT
     mesh_location: Path
     field_file_location: Optional[Path] = None
+
+    # TODO uncomment after move to pydantic v2
+    # @computed_field
+    # @cached_property
+    # def raw_capacitance_matrix(self) -> ndarray:
+    #     n = int(sqrt(len(self.capacitance_matrix)))
+    #     matrix = zeros((n, n))
+
+    #     port_to_index_map = {}
+    #     for iname, jname in self.capacitance_matrix.keys():
+    #         if iname not in port_to_index_map:
+    #             port_to_index_map[iname] = len(port_to_index_map) + 1
+    #         if jname not in port_to_index_map:
+    #             port_to_index_map[jname] = len(port_to_index_map) + 1
+
+    #     for (iname, jname), c in self.capacitance_matrix.items():
+    #         matrix[port_to_index_map[iname], port_to_index_map[jname]] = c
+
+    #     return matrix
 
 
 def _generate_sif(
@@ -33,21 +57,26 @@ def _generate_sif(
     name: str,
     signals: Sequence[str],
     bodies: Dict[str, Dict[str, Any]],
+    metal_layers: Iterable[str],
     layer_stack: LayerStack,
     material_spec: MaterialSpec,
     element_order: int,
+    background_tag: Optional[str] = None,
 ):
+    # pylint: disable=unused-argument
     """Generates a sif file for Elmer simulations using Jinja2."""
-    used_materials = {v.material for _, v in layer_stack.layers.items()}
+    used_materials = {v.material for v in layer_stack.layers.values()} | {
+        background_tag or set()
+    }
     used_materials = {
         k: material_spec[k]
         for k in used_materials
         if isfinite(material_spec[k].get("relative_permittivity", inf))
     }
+
     sif_template = Environment(
         loader=FileSystemLoader(ELECTROSTATIC_TEMPLATE.parent)
     ).get_template(ELECTROSTATIC_TEMPLATE.name)
-
     output = sif_template.render(**locals())
     with open(simulation_folder / f"{name}.sif", "w", encoding="utf-8") as fp:
         fp.write(output)
@@ -60,9 +89,9 @@ def _elmergrid(simulation_folder: Path, name: str, n_processes: int = 1):
         raise RuntimeError(
             "ElmerGrid not found. Make sure it is available in your PATH."
         )
-    with open(simulation_folder / f"{name}_ElmerGrid.log", encoding="utf-8") as fp:
+    with open(simulation_folder / f"{name}_ElmerGrid.log", "w", encoding="utf-8") as fp:
         subprocess.run(
-            [elmergrid, "14", "2", f"{name}.msh"],
+            [elmergrid, "14", "2", name],
             cwd=simulation_folder,
             shell=False,
             stdout=fp,
@@ -75,7 +104,7 @@ def _elmergrid(simulation_folder: Path, name: str, n_processes: int = 1):
                     elmergrid,
                     "2",
                     "2",
-                    f"{name}/",
+                    f"{Path(name).stem}/",
                     "-metiskway",
                     str(n_processes),
                     "4",
@@ -98,10 +127,13 @@ def _elmersolver(simulation_folder: Path, name: str, n_processes: int = 1):
     )
     if elmersolver is None:
         raise RuntimeError(
-            "ElmerSolver not found. Make sure it is available in your PATH."
+            ("ElmerSolver" if n_processes == 1 else "ElmerSolver_mpi")
+            + " not found. Make sure it is available in your PATH."
         )
-    sif_file = simulation_folder / f"{name}.sif"
-    with open(simulation_folder / f"{name}_ElmerSolver.log", encoding="utf-8") as fp:
+    sif_file = str(simulation_folder / f"{Path(name).stem}.sif")
+    with open(
+        simulation_folder / f"{name}_ElmerSolver.log", "w", encoding="utf-8"
+    ) as fp:
         subprocess.run(
             [elmersolver, sif_file]
             if no_mpi
@@ -115,13 +147,31 @@ def _elmersolver(simulation_folder: Path, name: str, n_processes: int = 1):
 
 
 def _read_elmer_results(
-    simulation_folder: Path, mesh_filename: str
+    simulation_folder: Path,
+    mesh_filename: str,
+    n_processes: int,
+    ports: Iterable[str],
 ) -> ElectrostaticResults:
     """Fetch results from successful Elmer simulations."""
-    # todo pandas or numpy ?
+    raw_name = Path(mesh_filename).stem
+    raw_capacitance_matrix = read_csv(
+        simulation_folder / f"{raw_name}_capacitance.dat",
+        sep=r"\s+",
+        header=None,
+        dtype=float,
+    ).values
     return ElectrostaticResults(
-        capacitance_matrix=[[1e-15]], mesh_location=simulation_folder / mesh_filename
-    )  # debug
+        capacitance_matrix={
+            (iname, jname): raw_capacitance_matrix[i][j]
+            for (i, iname), (j, jname) in itertools.product(
+                enumerate(ports), enumerate(ports)
+            )
+        },
+        mesh_location=simulation_folder / mesh_filename,
+        field_file_location=simulation_folder
+        / raw_name
+        / f'{raw_name}_t0001.{"pvtu" if n_processes > 1 else "vtu"}',
+    )
 
 
 def run_capacitive_simulation(
@@ -138,7 +188,7 @@ def run_capacitive_simulation(
     Returns the field solution and resulting capacitance matrix.
 
     Args:
-        component: Simulation envitonment as a gdsfactory component.
+        component: Simulation environment as a gdsfactory component.
         element_order:
             Order of polynomial basis functions.
             Higher is more accurate but takes more memory and time to run.
@@ -187,32 +237,53 @@ def run_capacitive_simulation(
         layer_stack=layer_stack,
         **(mesh_parameters or {}),
     )
-    # TODO read Physical groups from the mesh or netlist
 
     # Signals are converted to Elmer Boundary Conditions
-    signals = ["core"]  # todo infer from mesh
-    # non-signals are converted to Elmer Bodies
-    # TODO infer also
-    # metal without signal (port) should be grounded
-    bodies = {"vacuum": {"material": "vacuum"}, "substrate": {"material": "si"}}
+    # TODO infer port delimiter from somewhere
+    port_delimiter = "__"
+    signals = [
+        next(k for k, v in layer_stack.layers.items() if v.layer == port.layer)
+        + f"{port_delimiter}{port.name}"  # find first match
+        for port in component.get_ports()
+    ]
+    metal_layers = {
+        next(k for k, v in layer_stack.layers.items() if v.layer == port.layer)
+        for port in component.get_ports()
+    }  # assume ports are only on metal layers
+    # dielectrics
+    bodies = {
+        k: {"material": v.material}
+        for k, v in layer_stack.layers.items()
+        if port_delimiter not in k and k not in metal_layers
+    }
+    if background_tag := (mesh_parameters or {}).get("background_tag", None):
+        bodies = {**bodies, background_tag: {"material": background_tag}}
 
     _generate_sif(
         simulation_folder,
         component.name,
         signals,
         bodies,
+        metal_layers,
         layer_stack,
         material_spec,
         element_order,
+        background_tag,
     )
-    # _elmergrid(simulation_folder, filename, n_processes)
-    # _elmersolver(simulation_folder, filename, n_processes)
-    return _read_elmer_results(simulation_folder, filename)
+    _elmergrid(simulation_folder, filename, n_processes)
+    _elmersolver(simulation_folder, filename, n_processes)
+    return _read_elmer_results(
+        simulation_folder, filename, n_processes, component.ports
+    )
 
 
 if __name__ == "__main__":
+    import pyvista as pv
+
     from gdsfactory.technology.layer_stack import LayerLevel
     from gdsfactory.generic_tech import LAYER
+
+    # TODO make the example something functional
 
     # Example LayerStack values from doi:10.1103/PRXQuantum.4.010314 and
     layer_stack = LayerStack(
@@ -252,3 +323,6 @@ if __name__ == "__main__":
     # mesh
     results = run_capacitive_simulation(c)
     print(results)
+
+    field = pv.read(results.field_file_location)
+    field.plot(scalars="electric field", cmap="turbo")
